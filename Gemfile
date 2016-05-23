c275b014837e7b3a6f7cfc33ae3aeae9ccf47830
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-23 11:19:24 -0600

    first route

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.3.1'
+
+gem 'pry', '~> 0.10.3'
+gem 'sinatra', '~> 1.4.7'
+
--- redbox.rb
+++ redbox.rb
@@ -1,1 +1,16 @@
 # Simple rebox sinatra app
+
+require 'sinatra'
+require 'pry'
+
+
+get '/' do 
+	"Welcome to Redbox!"
+end
+
+
+# Menu
+# 1) List of titles
+# 2) Search
+# 3) Go to imdb
+
