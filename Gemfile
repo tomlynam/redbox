8caba1ca13951b47417f39dd5447e1d7074c46be
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-23 12:42:44 -0600

    print hash of titles

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ ruby '2.3.1'
 
 gem 'pry', '~> 0.10.3'
 gem 'sinatra', '~> 1.4.7'
-
--- redbox.rb
+++ redbox.rb
@@ -1,16 +1,59 @@
-# Simple rebox sinatra app
-
 require 'sinatra'
 require 'pry'
 
 
 get '/' do 
-	"Welcome to Redbox!"
+	erb :index
 end
 
 
-# Menu
-# 1) List of titles
-# 2) Search
-# 3) Go to imdb
+get '/titles' do
+	@titles = { 
+	 the_revenant: { 
+	   name: 'The Revenant', year: 2015, rating: 5
+	 },
+	 room: {
+	   name: 'Room', year: 2015, rating: 5
+	 },
+	 mad_max: {
+	   name: 'Mad Max', year: 2015, rating: 5
+	 },
+	 bridge_of_spies: { 
+	   name: 'Bridge of Spies', year: 2015, rating: 5
+	 },
+	 spotlight: { 
+	   name: 'Spotlight', year: 2015, rating: 5 
+	 },
+	 the_big_short: {
+	   name: 'The Big Short', year: 2015, rating: 5
+	 },
+	 the_martian: {
+	   name: 'The Martian', year: 2015, rating: 5
+	 },
+	 steve_jobs: {
+	   name: 'Steve Jobs', year: 2015, rating: 5
+	 },
+	  brooklyn: {
+	   name: 'Brooklyn', year: 2015, rating: 5
+	 },
+	  furious_7: {
+	   name: 'Furious 7', year: 2015, rating: 3
+	 }
+	}
+	erb :titles
+end
+
+
+# get '/search' do
+# 	erb :search
+# end
+
+
+# get '/cart' do
+# end
+
+
+# post '/search' do
+# 	"add code here"
+# end
 
