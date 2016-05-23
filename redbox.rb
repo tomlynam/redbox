94c3385a2a7052d07f0322ae09478a60e57e8163
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-23 14:24:59 -0600

    changed to an array of hashes

--- redbox.rb
+++ redbox.rb
@@ -8,39 +8,20 @@ end
 
 
 get '/titles' do
-	@titles = { 
-	 the_revenant: { 
-	   name: 'The Revenant', year: 2015, rating: 5
-	 },
-	 room: {
-	   name: 'Room', year: 2015, rating: 5
-	 },
-	 mad_max: {
-	   name: 'Mad Max', year: 2015, rating: 5
-	 },
-	 bridge_of_spies: { 
-	   name: 'Bridge of Spies', year: 2015, rating: 5
-	 },
-	 spotlight: { 
-	   name: 'Spotlight', year: 2015, rating: 5 
-	 },
-	 the_big_short: {
-	   name: 'The Big Short', year: 2015, rating: 5
-	 },
-	 the_martian: {
-	   name: 'The Martian', year: 2015, rating: 5
-	 },
-	 steve_jobs: {
-	   name: 'Steve Jobs', year: 2015, rating: 5
-	 },
-	  brooklyn: {
-	   name: 'Brooklyn', year: 2015, rating: 5
-	 },
-	  furious_7: {
-	   name: 'Furious 7', year: 2015, rating: 3
-	 }
-	}
+	@titles = [ 
+	  { name: 'The Revenant', year: 2015, rating: 5 },
+	  { name: 'Room', year: 2015, rating: 5 },
+	  { name: 'Mad Max', year: 2015, rating: 5 },
+	  { name: 'Bridge of Spies', year: 2015, rating: 5 },
+	  { name: 'Spotlight', year: 2015, rating: 5 },
+	  { name: 'The Big Short', year: 2015, rating: 5 },
+	  { name: 'The Martian', year: 2015, rating: 5 },
+	  { name: 'Steve Jobs', year: 2015, rating: 5 },
+	  { name: 'Brooklyn', year: 2015, rating: 5 },
+	  { name: 'Furious 7', year: 2015, rating: 3 }
+	]
 	erb :titles
+	
 end
 
 
